04c4fa9a2bb5a3e1a526e3925b545d8b6350605d
Matthias Bathke <matthias-bathke@straightvisions.com>
2022-08-30 13:28:24 +0200

    rebuild

---
+++ lib/css/config/general.php
@@ -0,0 +1,47 @@
+<?php
+	$properties					                                 = array();
+	$properties[$module->get_setting('position')->get_data()]	 = '5%';
+
+	echo $_s->build_css(
+		'#sv100_companion_sv_scroll_to_top',
+		array_merge(
+			$properties,
+			$module->get_setting('bg_color')->get_css_data('background-color'),
+			$module->get_setting('icon_size')->get_css_data('width', '', 'px'),
+			$module->get_setting('icon_size')->get_css_data('height', '', 'px')
+		)
+	);
+
+	echo $_s->build_css(
+		'#sv100_companion_sv_scroll_to_top:hover, #sv100_companion_sv_scroll_to_top:focus',
+		array_merge(
+			$module->get_setting('bg_color_hover')->get_css_data('background-color'),
+			$module->get_setting('icon_size_hover')->get_css_data('width', '', 'px'),
+			$module->get_setting('icon_size_hover')->get_css_data('height', '', 'px')
+		)
+	);
+
+	$properties						= array();
+	$icon = array_map(function ($val) {
+		if(strlen($val) > 0){
+			return 'url(\'data:image/svg+xml;utf8;base64,'.base64_encode($val).'\')';
+		}
+		return '';
+	}, $module->get_setting('icon')->get_data());
+
+	$properties['-webkit-mask-image']	= $icon;
+
+	echo $_s->build_css(
+		'#sv100_companion_sv_scroll_to_top > i',
+		array_merge(
+			$properties,
+			$module->get_setting('icon_color')->get_css_data('background-color')
+		)
+	);
+
+	echo $_s->build_css(
+		'#sv100_companion_sv_scroll_to_top:hover > i, #sv100_companion_sv_scroll_to_top:focus > i ',
+		array_merge(
+			$module->get_setting('icon_color_hover')->get_css_data('background-color')
+		)
+	);
--- sv_scroll_to_top.php
+++ sv_scroll_to_top.php
@@ -3,31 +3,22 @@
 
 	class sv_scroll_to_top extends modules {
 		public function init() {
-			$this->load_settings()
-				 ->register_scripts()
-				 ->set_section_title( __( 'Scroll To Top', 'sv100_companion' ) )
+			$this->set_section_title( __( 'Scroll To Top', 'sv100_companion' ) )
 				 ->set_section_desc( __( 'Settings', 'sv100_companion' ) )
-				 ->set_section_type( 'settings' )
-				 ->set_section_template_path( $this->get_path( 'lib/backend/tpl/settings.php' ) )
-				 ->get_root()
-				 ->add_section( $this );
+				 ->set_section_template_path( $this->get_path( 'lib/tpl/settings/init.php' ) )
+				 ->load_settings()
+			 	 ->register_scripts()
+				 ->get_root()->add_section( $this );
 
 			// Loads SV Scroll To Top
-			if (
-				$this->get_setting( 'activate' )->get_data() === '1'
-			) {
-				add_action( 'wp_footer', function(){
-					echo $this->load();
-				} , 1);
+			if ( $this->get_setting( 'activate' )->get_data() === '1' ) {
+				add_action( 'wp_footer', array($this, 'load') , 1);
 			}
 		}
 	
 		protected function load_settings(): sv_scroll_to_top {
-			//$this->get_settings_component( 'bg_color','background_color', '#1e1e1e' );
-			
 			$this->get_setting( 'activate' )
 				 ->set_title( __( 'Activate Scroll To Top Button', 'sv100_companion' ) )
-				 ->set_default_value( 0 )
 				 ->load_type( 'checkbox' );
 
 			$this->get_setting( 'position' )
@@ -43,16 +34,19 @@
 				 ->set_title( __( 'Icon embed code', 'sv100_companion' ) )
 				 ->set_description( __( 'Here you can post the SVG embed code.', 'sv100_companion' ) )
 				 ->set_default_value( '<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path d="M0 16.67l2.829 2.83 9.175-9.339 9.167 9.339 2.829-2.83-11.996-12.17z"/></svg>' )
+				 ->set_is_responsive(true)
 				 ->load_type( 'textarea' );
 
 			$this->get_setting( 'bg_color' )
 				->set_title( __( 'Background color', 'sv100_companion' ) )
-				->set_default_value( '#000' )
+				->set_default_value( '#000000' )
+				->set_is_responsive(true)
 				->load_type( 'color' );
 			
 			$this->get_setting( 'bg_color_hover' )
 				 ->set_title( __( 'Background color (Hover/Focus)', 'sv100_companion' ) )
-				 ->set_default_value( '#000' )
+				 ->set_default_value( '#000000' )
+				 ->set_is_responsive(true)
 				 ->load_type( 'color' );
 			
 			$this->get_setting( 'icon_size' )
@@ -61,6 +55,7 @@
 				 ->set_default_value( '100' )
 				 ->set_min( 0 )
 				 ->set_max( 100 )
+				 ->set_is_responsive(true)
 				 ->load_type( 'number' );
 			
 			$this->get_setting( 'icon_size_hover' )
@@ -69,16 +64,19 @@
 				 ->set_default_value( '100' )
 				 ->set_min( 0 )
 				 ->set_max( 100 )
+				 ->set_is_responsive(true)
 				 ->load_type( 'number' );
 			
 			$this->get_setting( 'icon_color' )
 				 ->set_title( __( 'Icon color', 'sv100_companion' ) )
 				 ->set_default_value( '#ffffff' )
+				 ->set_is_responsive(true)
 				 ->load_type( 'color' );
 			
 			$this->get_setting( 'icon_color_hover' )
 				 ->set_title( __( 'Icon color (Hover/Focus)', 'sv100_companion' ) )
 				 ->set_default_value( '#ffffff' )
+				 ->set_is_responsive(true)
 				 ->load_type( 'color' );
 	
 			return $this;
@@ -87,47 +85,28 @@
 		protected function register_scripts(): sv_scroll_to_top {
 			// Register Styles
 			$this->get_script( 'common' )
-				 ->set_path( 'lib/frontend/css/common.css' );
+				 ->set_path( 'lib/css/common/common.css' );
 	
 			// Register Scripts
 			$this->get_script( 'default_js' )
-				 ->set_path( 'lib/frontend/js/default.js' )
+				 ->set_path( 'lib/js/frontend/default.js' )
 				 ->set_type( 'js' );
-			
-			$this->get_script( 'inline_config' )
-				 ->set_path( 'lib/frontend/css/config.php' )
-				 ->set_inline( true );
+
+			$this->get_script( 'config' )
+			     ->set_path( 'lib/css/config/init.php' )
+			     ->set_inline( true );
 	
 			return $this;
 		}
-	
-		public function load( $settings = array() ): string {
-			$settings								= shortcode_atts(
-				array(
-					'inline'						=> true,
-				),
-				$settings,
-				$this->get_module_name()
-			);
-			
-			return $this->router( $settings );
-		}
-	
-		protected function router( array $settings ): string {
-			$output = '';
-			
-			if ( $this->get_setting( 'activate' )->get_data() === '1' ) {
-				ob_start();
-				$this->get_script( 'common' )->set_is_enqueued();
-				$this->get_script( 'default_js' )->set_is_enqueued();
-				$this->get_script( 'inline_config' )->set_is_enqueued();
-				
-				echo '<div id="' . $this->get_prefix() . '"><i></i></div>';
-				
-				$output	= ob_get_contents();
-				ob_end_clean();
+		public function enqueue_scripts(): sv_scroll_to_top {
+			foreach($this->get_scripts() as $script){
+				$script->set_is_enqueued();
 			}
-	
-			return $output;
+
+			return $this;
+		}
+		public function load() {
+			$this->enqueue_scripts();
+			echo '<div id="' . $this->get_prefix() . '"><i></i></div>';
 		}
 	}
